5b00ba9b98ab92240a076e546d7ec388a7df7cbc
JavScraper <javscraper@gmail.com>
2020-01-21 00:02:39 +0800

    mgstage.com 加入年龄认证Cookies

--- Emby.Plugins.JavScraper/Scrapers/JsProxyHttpClientHandler.cs
+++ Emby.Plugins.JavScraper/Scrapers/JsProxyHttpClientHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -41,6 +42,10 @@ namespace Emby.Plugins.JavScraper.Scrapers
                 if (!(url?.IndexOf("?") > 0))
                     request.Headers.Referrer = new Uri("https://www.google.com/?--ver=110&--mode=navigate&--type=document&origin=&--aceh=1&dnt=1&upgrade-insecure-requests=1&cookie=has_recent_activity%3D1%3B+has_recent_activity%3D1&--level=0"); ;
             }
+
+            //mgstage.com 加入年龄认证Cookies
+            if (url.Contains("mgstage.com") && !(request.Headers.TryGetValues("Cookie", out var cookies) && cookies.Contains("abc=1"))) 
+                request.Headers.Add("Cookie", "adc=1");
 
             // Add UserAgent
             request.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
