d6be24d2a8781828aace6ac4b27e5b107590f51f
JavScraper <javscraper@gmail.com>
2020-01-20 22:58:20 +0800

    增加 UserAgent

--- Emby.Plugins.JavScraper/Scrapers/JsProxyHttpClientHandler.cs
+++ Emby.Plugins.JavScraper/Scrapers/JsProxyHttpClientHandler.cs
@@ -41,6 +41,10 @@ namespace Emby.Plugins.JavScraper.Scrapers
                 if (!(url?.IndexOf("?") > 0))
                     request.Headers.Referrer = new Uri("https://www.google.com/?--ver=110&--mode=navigate&--type=document&origin=&--aceh=1&dnt=1&upgrade-insecure-requests=1&cookie=has_recent_activity%3D1%3B+has_recent_activity%3D1&--level=0"); ;
             }
+
+            // Add UserAgent
+            request.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+
             var resp = await base.SendAsync(request, cancellationToken);
 
             //不需要重定向
