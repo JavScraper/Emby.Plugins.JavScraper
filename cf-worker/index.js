9a03a7751cb264570f60a2932709d844be1a3cfe
JavScraper <javscraper@gmail.com>
2020-01-25 23:12:08 +0800

    简化 index.js

--- cf-worker/index.js
+++ cf-worker/index.js
@@ -1,21 +1,13 @@
 'use strict'
 
-/**
- * static files (404.html, sw.js, conf.js)
- */
-const ASSET_URL = 'https://etherdream.github.io/jsproxy'
-
-const JS_VER = 10
-const MAX_RETRY = 2
-
 /** @type {RequestInit} */
 const PREFLIGHT_INIT = {
-  status: 204,
-  headers: new Headers({
-    'access-control-allow-origin': '*',
-    'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
-    'access-control-max-age': '1728000',
-  }),
+    status: 204,
+    headers: new Headers({
+        'access-control-allow-origin': '*',
+        'access-control-allow-methods': 'GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS',
+        'access-control-max-age': '1728000',
+    }),
 }
 
 /**
@@ -24,87 +16,41 @@ const PREFLIGHT_INIT = {
  * @param {Object<string, string>} headers
  */
 function makeRes(body, status = 200, headers = {}) {
-  headers['--ver'] = JS_VER
-  headers['access-control-allow-origin'] = '*'
-  return new Response(body, {status, headers})
+    headers['access-control-allow-origin'] = '*'
+    return new Response(body, { status, headers })
 }
-
 
 /**
  * @param {string} urlStr 
  */
 function newUrl(urlStr) {
-  try {
-    return new URL(urlStr)
-  } catch (err) {
-    return null
-  }
+    try {
+        return new URL(urlStr)
+    } catch (err) {
+        return null
+    }
 }
 
-
 addEventListener('fetch', e => {
-  const ret = fetchHandler(e)
-    .catch(err => makeRes('cfworker error:\n' + err.stack, 502))
-  e.respondWith(ret)
+    const ret = fetchHandler(e)
+        .catch(err => makeRes('cfworker error:\n' + err.stack, 502))
+    e.respondWith(ret)
 })
-
 
 /**
  * @param {FetchEvent} e 
  */
 async function fetchHandler(e) {
-  const req = e.request
-  const urlStr = req.url
-  const urlObj = new URL(urlStr)
-  const path = urlObj.href.substr(urlObj.origin.length)
+    const req = e.request
+    const urlStr = req.url
+    const urlObj = new URL(urlStr)
+    const path = urlObj.href.substr(urlObj.origin.length)
 
-  if (urlObj.protocol === 'http:') {
-    urlObj.protocol = 'https:'
-    return makeRes('', 301, {
-      'strict-transport-security': 'max-age=99999999; includeSubDomains; preload',
-      'location': urlObj.href,
-    })
-  }
-
-  if (path.startsWith('/http/')) {
-    return httpHandler(req, path.substr(6))
-  }
-  if (path.startsWith('/cover/')) {
-    return httpCoverHandler(req, path.substr(7))
-  }
-  return makeRes('hello!')
-}
-
-/**
- * @param {Request} req
- * @param {string} pathname
- */
-function httpCoverHandler(req, pathname) {
-  // cfworker 会把路径中的 `//` 合并成 `/`
-  const imageURL = pathname.replace(/^(https?):\/+/, '$1://')
-  const imageRequest = new Request(imageURL, {
-    headers: req.headers,
-  });
-  let options = {
-  cf: {
-    image: {
-      width: 800,
-      height: 600,
-      draw: [{
-        url: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png", // draw this image
-        bottom: 5, // 5 pixels from the bottom edge
-        right: 5, // 5 pixels from the right edge
-        fit: "contain", // make it fit within 100x50 area
-        width: 100,
-        height: 50,
-        opacity: 0.8, // 20% transparent
-      }]
+    if (path.startsWith('/http/')) {
+        return httpHandler(req, path.substr(6))
     }
-  }
-}
- // options.cf.image.width ='360'
-  // return makeRes('hel2222lo!')
-  return fetch(imageRequest, options);
+
+    return makeRes('hello!')
 }
 
 /**
@@ -112,207 +58,61 @@ function httpCoverHandler(req, pathname) {
  * @param {string} pathname
  */
 function httpHandler(req, pathname) {
-  const reqHdrRaw = req.headers
-  if (reqHdrRaw.has('x-jsproxy')) {
-    return Response.error()
-  }
-
-  // preflight
-  if (req.method === 'OPTIONS' &&
-      reqHdrRaw.has('access-control-request-headers')
-  ) {
-    return new Response(null, PREFLIGHT_INIT)
-  }
-
-  let acehOld = false
-  let rawSvr = ''
-  let rawLen = ''
-  let rawEtag = ''
-
-  const reqHdrNew = new Headers(reqHdrRaw)
-  reqHdrNew.set('x-jsproxy', '1')
-  //reqHdrNew.set("X-Forwarded-For","1.1.1.1,1.0.0.1")
-
-  // 此处逻辑和 http-dec-req-hdr.lua 大致相同
-  // https://github.com/EtherDream/jsproxy/blob/master/lua/http-dec-req-hdr.lua
-  let refer = reqHdrNew.get('referer')
-  if(!refer || refer.indexOf('?')<0)
-  {
-    refer="https://www.google.com/?--ver=110&--mode=navigate&--type=document&origin=&--aceh=1&dnt=1&upgrade-insecure-requests=1&cookie=has_recent_activity%3D1%3B+has_recent_activity%3D1&--level=0"
-  }
-  const query = refer.substr(refer.indexOf('?') + 1)
-  if (!query) {
-    return makeRes('missing params', 403)
-  }
-  const param = new URLSearchParams(query)
-
-  for (const [k, v] of Object.entries(param)) {
-    if (k.substr(0, 2) === '--') {
-      // 系统信息
-      switch (k.substr(2)) {
-      case 'aceh':
-        acehOld = true
-        break
-      case 'raw-info':
-        [rawSvr, rawLen, rawEtag] = v.split('|')
-        break
-      }
-    } else {
-      // 还原 HTTP 请求头
-      if (v) {
-        reqHdrNew.set(k, v)
-      } else {
-        reqHdrNew.delete(k)
-      }
+    const reqHdrRaw = req.headers
+    if (reqHdrRaw.has('x-jsproxy')) {
+        return Response.error()
     }
-  }
-  if (!param.has('referer')) {
-    reqHdrNew.delete('referer')
-  }
 
-  // cfworker 会把路径中的 `//` 合并成 `/`
-  const urlStr = pathname.replace(/^(https?):\/+/, '$1://')
-  const urlObj = newUrl(urlStr)
-  if (!urlObj) {
-    return makeRes('invalid proxy url: ' + urlStr, 403)
-  }
+    // preflight
+    if (req.method === 'OPTIONS' && reqHdrRaw.has('access-control-request-headers')) {
+        return new Response(null, PREFLIGHT_INIT)
+    }
 
-  /** @type {RequestInit} */
-  const reqInit = {
-    method: req.method,
-    headers: reqHdrNew,
-    redirect: 'manual',
-  }
-  if (req.method === 'POST') {
-    reqInit.body = req.body
-  }
-  return proxy(urlObj, reqInit, acehOld, rawLen, 0)
+    const reqHdrNew = new Headers(reqHdrRaw)
+    reqHdrNew.set('x-jsproxy', '1')
+
+    if (!reqHdrRaw.has('user-agent')) {
+        reqHdrRaw.set('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36')
+    }
+    // cfworker 会把路径中的 `//` 合并成 `/`
+    const urlStr = pathname.replace(/^(https?):\/+/, '$1://')
+    const urlObj = newUrl(urlStr)
+    if (!urlObj) {
+        return makeRes('invalid proxy url: ' + urlStr, 403)
+    }
+
+    /** @type {RequestInit} */
+    const reqInit = {
+        method: req.method,
+        headers: reqHdrNew,
+    }
+    if (req.method === 'POST') {
+        reqInit.body = req.body
+    }
+    return proxy(urlObj, reqInit)
 }
-
 
 /**
  * 
  * @param {URL} urlObj 
- * @param {RequestInit} reqInit 
- * @param {number} retryTimes 
+ * @param {RequestInit} reqInit
  */
-async function proxy(urlObj, reqInit, acehOld, rawLen, retryTimes) {
-  const res = await fetch(urlObj.href, reqInit)
-  const resHdrOld = res.headers
-  const resHdrNew = new Headers(resHdrOld)
+async function proxy(urlObj, reqInit) {
+    const res = await fetch(urlObj.href, reqInit)
+    const resHdrOld = res.headers
+    const resHdrNew = new Headers(resHdrOld)
 
-  let expose = '*'
-  
-  for (const [k, v] of resHdrOld.entries()) {
-    if (k === 'access-control-allow-origin' ||
-        k === 'access-control-expose-headers' ||
-        k === 'location' ||
-        k === 'set-cookie'
-    ) {
-      const x = '--' + k
-      resHdrNew.set(x, v)
-      if (acehOld) {
-        expose = expose + ',' + x
-      }
-      resHdrNew.delete(k)
-    }
-    else if (acehOld &&
-      k !== 'cache-control' &&
-      k !== 'content-language' &&
-      k !== 'content-type' &&
-      k !== 'expires' &&
-      k !== 'last-modified' &&
-      k !== 'pragma'
-    ) {
-      expose = expose + ',' + k
-    }
-  }
+    let status = res.status
 
-  if (acehOld) {
-    expose = expose + ',--s'
-    resHdrNew.set('--t', '1')
-  }
+    resHdrNew.set('access-control-expose-headers', '*')
+    resHdrNew.set('access-control-allow-origin', '*')
 
-  // verify
-  if (rawLen) {
-    const newLen = resHdrOld.get('content-length') || ''
-    const badLen = (rawLen !== newLen)
+    resHdrNew.delete('content-security-policy')
+    resHdrNew.delete('content-security-policy-report-only')
+    resHdrNew.delete('clear-site-data')
 
-    if (badLen) {
-      if (retryTimes < MAX_RETRY) {
-        urlObj = await parseYtVideoRedir(urlObj, newLen, res)
-        if (urlObj) {
-          return proxy(urlObj, reqInit, acehOld, rawLen, retryTimes + 1)
-        }
-      }
-      return makeRes(res.body, 400, {
-        '--error': `bad len: ${newLen}, except: ${rawLen}`,
-        'access-control-expose-headers': '--error',
-      })
-    }
-
-    if (retryTimes > 1) {
-      resHdrNew.set('--retry', retryTimes)
-    }
-  }
-
-  let status = res.status
-
-  resHdrNew.set('access-control-expose-headers', expose)
-  resHdrNew.set('access-control-allow-origin', '*')
-  resHdrNew.set('--s', status)
-  resHdrNew.set('--ver', JS_VER)
-
-  resHdrNew.delete('content-security-policy')
-  resHdrNew.delete('content-security-policy-report-only')
-  resHdrNew.delete('clear-site-data')
-
-  if (status === 301 ||
-      status === 302 ||
-      status === 303 ||
-      status === 307 ||
-      status === 308
-  ) {
-    status = status + 10
-  }
-
-  return new Response(res.body, {
-    status,
-    headers: resHdrNew,
-  })
-}
-
-
-/**
- * @param {URL} urlObj 
- */
-function isYtUrl(urlObj) {
-  return (
-    urlObj.host.endsWith('.googlevideo.com') &&
-    urlObj.pathname.startsWith('/videoplayback')
-  )
-}
-
-/**
- * @param {URL} urlObj 
- * @param {number} newLen 
- * @param {Response} res 
- */
-async function parseYtVideoRedir(urlObj, newLen, res) {
-  if (newLen > 2000) {
-    return null
-  }
-  if (!isYtUrl(urlObj)) {
-    return null
-  }
-  try {
-    const data = await res.text()
-    urlObj = new URL(data)
-  } catch (err) {
-    return null
-  }
-  if (!isYtUrl(urlObj)) {
-    return null
-  }
-  return urlObj
+    return new Response(res.body, {
+        status,
+        headers: resHdrNew,
+    })
 }
