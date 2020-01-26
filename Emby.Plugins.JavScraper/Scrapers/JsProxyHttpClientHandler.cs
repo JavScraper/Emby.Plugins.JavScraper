12f4d21292f7e67921581a3e1f7d4590684838e4
JavScraper <javscraper@gmail.com>
2020-01-26 09:41:02 +0800

    UserAgent 添加前判断

--- Emby.Plugins.JavScraper/Scrapers/JsProxyHttpClientHandler.cs
+++ Emby.Plugins.JavScraper/Scrapers/JsProxyHttpClientHandler.cs
@@ -54,7 +54,8 @@ namespace Emby.Plugins.JavScraper.Scrapers
                 request.Headers.Add("Cookie", "adc=1");
 
             // Add UserAgent
-            request.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+            if (!(request.Headers.UserAgent?.Count() > 0))
+                request.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
 
             return base.SendAsync(request, cancellationToken);
         }
