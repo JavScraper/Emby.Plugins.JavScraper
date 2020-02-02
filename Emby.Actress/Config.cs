db69be370b9f9f3886cb5f47fe4e28d92bd18f9b
JavScraper <javscraper@gmail.com>
2020-02-02 14:04:08 +0800

    批量导入女优头像

---
+++ Emby.Actress/Config.cs
@@ -0,0 +1,23 @@
+﻿namespace Emby.Actress
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// Emby 站点
+        /// </summary>
+        public string url { get; set; } = "http://localhost:8096/";
+
+        /// <summary>
+        /// Api Key
+        /// </summary>
+        public string api_key { get; set; } = "";
+
+        /// <summary>
+        /// 头像文件夹
+        /// </summary>
+        public string dir { get; set; } = "女优头像";
+    }
+}
